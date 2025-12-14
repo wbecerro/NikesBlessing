6a083b298fa1f7058ecbf0da1093598500db1336
wbe <34304532+wbecerro@users.noreply.github.com>
2025-12-14 13:54:54 +0100

    Commit inicial.

---
+++ src/main/java/wbe/nikesBlessing/NikesBlessing.java
@@ -0,0 +1,17 @@
+package wbe.nikesBlessing;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class NikesBlessing extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
